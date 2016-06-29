0f85aae618d190056516050ec5e49977571d0b57
weilai <weilai.wl@alibaba-inc.com>
2016-06-29 10:40:33 +0800

    webpack build

--- app/view/main.js
+++ app/view/main.js
@@ -1,5 +1,5 @@
 
 import React from 'react'
-import Hello from '$com/hello'
+import Hello from '@com/hello'
 
 React.render(<Hello />, document.getElementById('app'))
--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,15 @@
 
 const webpack = require('webpack')
 const path = require('path')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
-  entry: path.resolve(__dirname, 'app/view/main.js'),
+  entry: {
+    main: path.resolve(__dirname, 'app/view/main.js')
+  },
   output: {
-    path: 'app/public/',
-    filename: 'bundle.js'
+    path: '.build',
+    filename: '/app/public/[name].js'
   },
   resolve: {
     root: [
@@ -15,7 +18,7 @@ module.exports = {
     ],
     extensions: ['', '.js', '.jsx', '.json'],
     alias: {
-      '$com': path.resolve(__dirname, 'app/view/component')
+      '@com': path.resolve(__dirname, 'app/view/component')
     }
   },
   module: {
@@ -42,5 +45,25 @@ module.exports = {
     'react': 'React',
     'react-dom': 'ReactDom'
   },
-  plugins: []
+  plugins: [
+    new CopyWebpackPlugin([
+      {
+        from: {
+          glob: 'app/**/*',
+          dot: false
+        },
+        ignore: 'app/view/**/*'
+      },
+      {
+        from: 'app/view/**/*.html'
+      },
+      {
+        from: 'app/view/lib',
+        to: 'app/public/lib',
+      },
+      { from: 'config/**/*' },
+      { from: '*.json' },
+      { from: 'Procfile' }
+    ])
+  ]
 }
