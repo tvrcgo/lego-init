215dd791ccdb2f5bed9586994acc8622444cce9f
tvrcgo <tvrcgo@gmail.com>
2016-07-16 19:16:08 +0800

    lego-static standalone.

--- app/plugin/lego-static/index.js
+++ app/plugin/lego-static/index.js
@@ -6,6 +6,6 @@ const resolve = require('path').resolve
 
 module.exports = (opts, mnt, app) => {
   // static server middleware
-  const root = resolve(__dirname, '../../public')
+  const root = resolve(process.cwd(), 'app/public')
   return mount('/public', serve(root))
 }
